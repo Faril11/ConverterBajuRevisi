28d1265a314e5fada828cc1667be2fd53d12fd09
Fazrul Ridha Alliandre <fazrulalliandre357@gmail.com>
2024-06-23 13:40:30 +0700

    Fazrul Ridha Alliandre_6706220006 revisi assesment 1 selesai

--- app/src/main/java/org/d3if0006/converterbajurevisi/navigation/NavGraph.kt
+++ app/src/main/java/org/d3if0006/converterbajurevisi/navigation/NavGraph.kt
@@ -7,7 +7,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.d3if0006.converterbajurevisi.ui.screen.AboutScreen
 import org.d3if0006.converterbajurevisi.ui.screen.MainScreen
-
+//commit
 @Composable
 fun SetupNavGraph(navController: NavHostController= rememberNavController()) {
     NavHost(navController = navController,
